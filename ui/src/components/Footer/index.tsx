d610dbfd1a3fa81dd3e8ec495a6ba9b3ae361b7f
Rael CALITRO <rael06@hotmail.fr>
2024-02-08 12:22:16 +0100

    improve footer

--- ui/src/components/Footer/index.tsx
+++ ui/src/components/Footer/index.tsx
@@ -5,21 +5,23 @@ import classes from "./classes.module.css";
 function Footer() {
   return (
     <Box className={classes.root}>
-      <Typography variant="body2" fontStyle="italic">
-        Developper:{" "}
-      </Typography>
-      <a href="https://rael-calitro.ovh" target="_blank" rel="noreferrer">
-        Rael CALITRO
-      </a>
+      <Box className={classes.link}>
+        <Typography variant="body2" fontStyle="italic">
+          Developper:{" "}
+        </Typography>
+        <a href="https://rael-calitro.ovh" target="_blank" rel="noreferrer">
+          Rael CALITRO
+        </a>
+      </Box>
 
-      {" - "}
-
-      <Typography variant="body2" fontStyle="italic">
-        Using open source LLM Model from:{" "}
-      </Typography>
-      <a href="https://mistral.ai" target="_blank" rel="noreferrer">
-        Mistral AI
-      </a>
+      <Box className={classes.link}>
+        <Typography variant="body2" fontStyle="italic">
+          Using open source LLM Model from:{" "}
+        </Typography>
+        <a href="https://mistral.ai" target="_blank" rel="noreferrer">
+          Mistral AI
+        </a>
+      </Box>
     </Box>
   );
 }
