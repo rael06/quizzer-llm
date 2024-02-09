ea8f0bc81c3d19b8a1ad1743f2503f7fdf7eee1a
Rael Calitro <rael06@hotmail.fr>
2024-02-09 13:01:02 +0100

    improve deploy script

--- deploy.js
+++ deploy.js
@@ -16,26 +16,37 @@ const runCommand = (command) => {
 	try {
 		console.info("Deploy started..");
 
+		console.info("\nStep 1/6");
+		console.info("Pull started..");
 		await runCommand("git pull");
 		console.info("Pull executed successfully.");
 
+		console.info("\nStep 2/6");
 		console.info("Stop container started..");
 		await runCommand("docker stop quizzer-llm").catch(() => console.error("Error stopping container"));
 		console.info("Stop container successful.");
 
+		console.info("\nStep 3/6");
+		console.info("Remove container started..");
 		await runCommand("docker rm quizzer-llm").catch(() => console.error("Error removing container"));
 		console.info("Remove container successful.");
 
+		console.info("\nStep 4/6");
+		console.info("Remove image started..");
 		await runCommand("docker rmi quizzer-llm").catch(() => console.error("Error removing image"));
 		console.info("Remove image successful.");
 
+		console.info("\nStep 5/6");
 		console.info("Build started..");
 		await runCommand("docker build -t quizzer-llm .").catch((e) => console.error("Error building image", e));
 		console.info("Build successful.");
 
+		console.info("\nStep 6/6");
+		console.info("Run container started..");
 		await runCommand("docker run -d -p 3099:3099 --name quizzer-llm quizzer-llm").catch((e) => console.error("Error running container", e));
 		console.info("Run container successful.");
 
+		console.info("\n");
 		console.info("Deploy successful.");
 	} catch (error) {
 		console.error("An error occurred:", error);
